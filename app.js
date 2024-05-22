8bc6c7d93a3ef242537ee54b3dfca6a05c1f901b
Alessandro Campo <alessandrocampo97@gmail.com>
2024-05-22 15:36:46 +0200

    feat private properties

--- app.js
+++ app.js
@@ -3,29 +3,63 @@ const media = require('./media');
 
 class Movie {
 
+    #title
+    #year
+    #genre
+    #rating
+    #type
+
     constructor({ title, year, genre, rating, type }) {
-        this.title = title
-        this.year = year
-        this.genre = genre
-        this.rating = rating
-        this.type = type
+        this.#title = title
+        this.#year = year
+        this.#genre = genre
+        this.#rating = rating
+        this.#type = type
     }
 
     getInformations() {
-        return `${this.title} is a ${this.type} of the ${this.genre} genre. It was released in ${this.year} and has been rated ${this.rating} out of 10.`;
+        return `${this.#title} is a ${this.#type} of the ${this.#genre} genre. It was released in ${this.#year} and has been rated ${this.#rating} out of 10.`;
     }
+
+    getProperty(requst) {
+        switch (requst) {
+            case 'title':
+                return this.#title
+                break;
+            case 'year':
+                return this.#year
+                break;
+            case 'genre':
+                return this.#genre
+                break;
+            case 'rating':
+                return this.#rating
+                break;
+            case 'type':
+                return this.#type
+                break;
+
+            default: return 'No property found'
+
+        }
+    }
+
+
 }
 
 class TvShow extends Movie {
     #seasons
     constructor({ title, year, genre, rating, type, seasons }) {
         super({ title, year, genre, rating, type })
-        this.seasons = seasons
+        this.#seasons = seasons
     }
 
     getInformations() {
         return `${this.title} is a ${this.type} of the ${this.genre} genre. The first season aired in ${this.year} and it has a total of ${this.seasons} seasons. Viewers have rated it ${this.rating} out of 10.`;
+    }
 
+    get seasons() {
+        return this.#seasons
     }
 }
 
@@ -35,7 +69,7 @@ const arrayOfClassInstances = media.map(el => {
 
 const filterByGenre = (arr, genre) => {
     const filteredArray = arr.filter(media => {
-        return media.genre == genre
+        return media.getProperty('genre') == genre
     })
     return filteredArray
 }
@@ -44,25 +78,27 @@ const filterByGenre = (arr, genre) => {
 const ratingAverageGivenGenre = (arr, genre) => {
     const filteredArray = filterByGenre(arr, genre)
     const ratingAverage = filteredArray.reduce((acc, media, index, array) => {
-        return acc + media.rating / array.length;
+        return acc + media.getProperty('rating') / array.length;
     }, 0)
 
     return Math.round(ratingAverage)
 }
 
 
-const getAllMediasGenres = (arr) => {
-    const arrOfGenres = arr.reduce((acc, { genre }) => {
-        if (!acc.includes(genre)) {
-            acc.push(genre)
-        }
-        return acc
-    }, [])
+//NOTE - Dunno if I can use  this method with private property
 
-    return arrOfGenres
+// const getAllMediasGenres = (arr) => {
+//     const arrOfGenres = arr.reduce((acc, { genre }) => {
+//         if (!acc.includes(genre)) {
+//             acc.push(genre)
+//         }
+//         return acc
+//     }, [])
+
+//     return arrOfGenres
 
 
-}
+// }
 
 
 const getMediasInfosGivenGenre = (arr, genre) => {
@@ -72,6 +108,19 @@ const getMediasInfosGivenGenre = (arr, genre) => {
     })
     return arrayOfInformations
 }
+
+const getAllMediasGenres = (arr) => {
+    const allGenres = []
+    arr.forEach(media => {
+        const mediaGenre = media.getProperty('genre')
+        if (!allGenres.includes(mediaGenre)) {
+            allGenres.push(mediaGenre)
+        }
+    })
+    return allGenres
+}
+
+
 
 console.log(ratingAverageGivenGenre(arrayOfClassInstances, 'Crime'));
 console.log(getAllMediasGenres(arrayOfClassInstances))
